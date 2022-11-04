d26eb0f715e491f18623c2da409f8530cda1b129
chocoloe <xinyuh22@uw.edu>
2022-11-04 11:51:04 -0700

    set up router, create component folder

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter, Outlet } from 'react-router-dom';
 
+function App() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+
+    );
+}
+
+export default App;
