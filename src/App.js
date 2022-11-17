923baf4efad224393a9ef3d08a3c5599444c2810
SelenaAndo <selenaa1072@gmail.com>
2022-11-17 04:32:47 -0800

    create signIn page with firebase authentification code, install packages for router and firebase, edit new version of code for routing Selena and Brenda

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter, Outlet } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, Outlet, Link } from 'react-router-dom';
 
-function App() {
-
+function App(props) {
+    // const [currentUser, setCurrentUser] = useState()
     return (
         <BrowserRouter>
             <Routes>
-                
+                <Route path="homePage" element={<homePage />} />
+                <Route path="Journaling" element={<Journaling />} />
+                <Route path="signIn" element={<signIn />} />
+                <Route path="Questionnaire" element={<Questionnaire />} />
             </Routes>
         </BrowserRouter>
 
