e59dcb4fa64ecb55d2fccf66600b16536c380e2c
SelenaAndo <selenaa1072@gmail.com>
2022-11-17 14:13:32 -0800

    add descriptions and details in index.html, fix typo in journaling function, add routing code in app.js, and add code for firebase configuration and initialization (still working on deploying firebase as host)

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ function App(props) {
             <Routes>
                 <Route path="homePage" element={<homePage />} />
                 <Route path="Journaling" element={<Journaling />} />
-                <Route path="signIn" element={<signIn />} />
-                <Route path="Questionnaire" element={<Questionnaire />} />
-            </Routes>
+                <Route path="signIn" element={<signIn />} /> 
+                {/* <Route path="Questionnaire" element={<Questionnaire />} /> */}
+            </Routes> 
         </BrowserRouter>
 
     );
+    // should sign-in route be nested in the homepage route?
 }
 
 export default App;
