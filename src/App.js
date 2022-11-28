ca80c81f90f655aa774c62ff65bf0449d28a1e01
chocoloe <xinyuh22@uw.edu>
2022-11-28 14:57:46 -0800

    set up a form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter, Outlet } from 'react-router-dom';
+import Quiz from './components/Quiz';
 
 function App() {
 
     return (
         <BrowserRouter>
             <Routes>
-                
+                <Route path='/questionnaire' element={<Quiz />} />
             </Routes>
         </BrowserRouter>
 
