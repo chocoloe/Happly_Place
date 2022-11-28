03b0009cad6829cdf2b9683a2f368bd41059353a
Bobony <bobony@uw.edu>
2022-11-27 21:11:03 -0800

    deleted some SSI from index html and imported

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -1,4 +1,2 @@
 import React from 'react';
-import firebase from 'firebase/compat/app';
-import * as firebaseui from 'firebaseui'
-import 'firebaseui/dist/firebaseui.css'
+import { getAuth } from "firebase/auth";
