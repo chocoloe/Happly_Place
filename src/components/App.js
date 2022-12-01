5342fc874ca94c7e358781983ec4730d335fe67e
jacob-hitchcock <jhitch2@uw.edu>
2022-12-01 13:30:00 -0800

    styled journal

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Quiz from './Quiz';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Signin from './SignIn';
+// import Signin from './SignIn';
 import HomePage from './HomePage';
 import Results from './Results';
+import Journaling from './Journaling';
 
 
 function App(props) {
@@ -11,9 +12,10 @@ function App(props) {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/signin" element={<Signin />} />
+            {/* <Route path="/signin" element={<Signin />} /> */}
             <Route path="/quiz" element={<Quiz />} />
             <Route path="/results" element={<Results />} />
+            <Route path="/journaling" element={<Journaling />} />
         </Routes> 
     );
     // should sign-in route be nested in the homepage route?
