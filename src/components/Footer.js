7a33f543ebdec6f95974eadd086a401e0fc247c5
SelenaAndo <selenaa1072@gmail.com>
2022-11-30 18:30:12 -0800

    add footer code made by Brenda taht she wasn't able to add due to merge conflicts and other technical issues

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>Happy Place</p>
+                <address>
+                    Contact us at  <a href="tel:425-123-4567">(425)-123-4567</a>
+                </address>
+                <p>&copy; INFO 442 Fall 2022</p>
+        </footer>
+    )
+}
