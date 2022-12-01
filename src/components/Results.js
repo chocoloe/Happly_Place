a5d1140172e91c528d2b7a6c93129feb79a15085
rheaguliani2 <rheagul2@uw.edu>
2022-11-30 19:23:15 -0800

    add results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+
 export default function Results() {
+    
     return(
         <div>
             <span>This is the results page</span>
         </div>
+        
     );
+
 }
