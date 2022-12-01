0a5c41d5c4e71324ba04b6484a27e7250ffb3a87
SelenaAndo <selenaa1072@gmail.com>
2022-12-01 05:48:46 -0800

    add an image folder with images needed for homepage and finish up content for one section of homepage (the mental health stat section)

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,7 +13,7 @@ export default function HomePage() {
         <main>
             <section>
                 <h2>What is your focus for today?</h2>
-                <div>
+                <div className="container">
                     {/* <div className="search-sort-container">
                     <div className="search-bar">
                         <input type="text" placeholder="Company Name" onChange={handleChange} value={searchInput}/>
@@ -24,16 +24,36 @@ export default function HomePage() {
              </section>
             <section>
                 <h2>Underrepresented College Students and Mental Health </h2>
-                <div>
-                    {/* <div className="card-container">
-                        {companyCards}
-                    </div> */}
+                <div className="container">
+                    <div className="container-image">
+                        <img src="../img/College-Student-Mental-Health.jpg" alt="illustration of young women struggling with her mental health" />
+                    </div>
+                    <div>
+                        <h3>
+                            Quick Statistics and Facts 
+                        </h3>
+                        <p>
+                            There is a mental health crisis among college students: 'more than 60% of college students diagnosed with at least one mental health condition' - Healthy Minds Study
+                        </p>
+                        <p>
+                            Covid-19 introduced a additional set of challenges for college students to navigate through and exacerbated the effects of existing stressors: In a recent study by TimelyMD, they found that 70% of the 1,600 college students they surveyed experienced emotional distress from the COVID-19 pandemic. 
+                        </p>
+                        <p>
+                            COVID-19 related stress disproportionately affected students from underrepresented and marginalized communities:  76% of women and 81% of non-binary individuals were negatively impacted by COVID-19 related stress according to 
+                        </p>
+                        <p>
+                            However, some barriers that have prevented students from these communities in getting the treatment and support they need incude stigma, the need for mental health resources rapidly exceeding available resources, financial issues, and available resources not being a good fit (such as not being understanding of their unique struggles and lack of representation)
+                        </p>
+                    </div>
                 </div>
+                
             </section> 
-            <h2>About Us</h2>
-                 <p>Happy Place is an application that allows underrepresented students at the University of Washington to access quality mental health care by providing them with an outlet through a journal. </p>
-                 <p>This journal allows them to express their feelings and concerns and prompts them with questions through a guided questionnaire.  </p>
-                {/* <button className="btn btn-light home-link"><Link to="/">Start My Apartment Search</Link></button> */}
+            <section>
+                <h2>About Us</h2>
+                    <p>Happy Place is an application that allows underrepresented students at the University of Washington to access quality mental health care by providing them with an outlet through a journal. </p>
+                    <p>This journal allows them to express their feelings and concerns and prompts them with questions through a guided questionnaire.  </p>
+                    {/* <button className="btn btn-light home-link"><Link to="/">Start My Apartment Search</Link></button> */}
+            </section>
         </main>
         <footer>
             <Footer />
