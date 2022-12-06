e5e317409f2fc0c6a40129f268c5cb344dc9b365
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2022-12-06 01:41:21 -0800

    Added NavBar to journaling component

--- src/components/Journaling.js
+++ src/components/Journaling.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Alert } from 'reactstrap';
 import Entries from './Entries';
 import Form from './Form';
+import NavBar from './NavBar.js';
 
 class Journaling extends Component {
   state = {
@@ -28,6 +29,9 @@ class Journaling extends Component {
 
       return ( 
           <div className = "Journaling">
+            <header>
+              <NavBar />
+            </header>
             <h1 className="m-3" id="journalHead">Your Journal</h1> 
             <Alert className="ms-3 me-3" color="secondary">
               Journaling can help you better understand your mental health and gives you the opportunity to record your feelings privately. Remember, your journal entries are secure and can only be read by you!
