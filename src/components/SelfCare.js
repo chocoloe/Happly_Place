8428efaeb17b7dcbc31cd36846d49c75d9b04456
rheaguliani2 <rheagul2@uw.edu>
2022-12-07 22:04:29 -0800

    add alts to images

--- src/components/SelfCare.js
+++ src/components/SelfCare.js
@@ -19,7 +19,7 @@ export default function SelfCare() {
                     <CardBody>
                         <CardTitle tag="h5">Emotionally Restorative Self-Care for PoC</CardTitle>
                     </CardBody>
-                    <iframe src="https://www.youtube.com/embed/GuLT_YQLGF8" title="YouTube video player" frameborder="0" height="200px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe alt="self care youtube video" src="https://www.youtube.com/embed/GuLT_YQLGF8" title="YouTube video player" frameborder="0" height="200px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     <CardBody>
                         <CardText></CardText>
                     </CardBody>
@@ -28,7 +28,7 @@ export default function SelfCare() {
                     <CardBody>
                         <CardTitle tag="h5">Self-Care Tips for College Students</CardTitle>
                     </CardBody>
-                    <img alt="Sample" src="https://891327.smushcdn.com/2078204/wp-content/uploads/2021/12/Untitled-design-2021-12-06T205018.956.png?lossy=1&strip=1&webp=1" height="200px"/>
+                    <img alt="college students" src="https://891327.smushcdn.com/2078204/wp-content/uploads/2021/12/Untitled-design-2021-12-06T205018.956.png?lossy=1&strip=1&webp=1" height="200px"/>
                     <CardBody>
                         <Button href="https://whatcherithinks.com/25-self-care-tips-for-college-students/">Visit Site</Button>
                     </CardBody>
@@ -37,7 +37,7 @@ export default function SelfCare() {
                     <CardBody>
                         <CardTitle tag="h5">Serenity Space at the Samuel E. Kelly Ethnic Cultural Center </CardTitle>
                     </CardBody>
-                    <img alt="Sample" src="https://depts.washington.edu/ecc/wordpress/wp-content/uploads/2022/06/Serenity-1-500x300.jpg" height="200px"/>
+                    <img alt="self care room" src="https://depts.washington.edu/ecc/wordpress/wp-content/uploads/2022/06/Serenity-1-500x300.jpg" height="200px"/>
                     <CardBody>
                         <CardText></CardText>
                         <Button href="https://depts.washington.edu/ecc/serenity-space/">Visit Site</Button>
@@ -49,7 +49,7 @@ export default function SelfCare() {
                     <CardBody>
                         <CardTitle tag="h5">UW Recreation - Mindfulness, yoga and meditation</CardTitle>
                     </CardBody>
-                    <img alt="Sample" src="https://uw-s3-cdn.s3.us-west-2.amazonaws.com/wp-content/uploads/sites/37/2022/04/07110053/Mindfulness-webpage-spring-2022-landscape-3_ccexpress-750x500.jpeg" height="200px"/>
+                    <img alt="people doing yoga" src="https://uw-s3-cdn.s3.us-west-2.amazonaws.com/wp-content/uploads/sites/37/2022/04/07110053/Mindfulness-webpage-spring-2022-landscape-3_ccexpress-750x500.jpeg" height="200px"/>
                     <CardBody>
                         <CardText>
                             Discover ways to nurture your body and mind and experience more moments of reflection, clarity, empowerment and connection in your daily lives.
@@ -61,7 +61,7 @@ export default function SelfCare() {
                     <CardBody>
                         <CardTitle tag="h5">UW Pet Therapy</CardTitle>
                     </CardBody>
-                    <img alt="Sample" src="https://post.healthline.com/wp-content/uploads/2020/08/pet-therapy_thumb.jpg" height="200px"/>
+                    <img alt="dog" src="https://post.healthline.com/wp-content/uploads/2020/08/pet-therapy_thumb.jpg" height="200px"/>
                     <CardBody>
                         <CardText>
                             For those who would like to enjoy time with furry friends and destress.
@@ -73,7 +73,7 @@ export default function SelfCare() {
                     <CardBody>
                         <CardTitle tag="h5">UW Mindfulness Meditation group workshops</CardTitle>
                     </CardBody>
-                    <img alt="Sample" src="https://img.freepik.com/free-vector/organic-flat-people-meditating-illustration_23-2148911452.jpg?w=2000" height="200px"/>
+                    <img alt="person meditating" src="https://img.freepik.com/free-vector/organic-flat-people-meditating-illustration_23-2148911452.jpg?w=2000" height="200px"/>
                     <CardBody>
                         <CardText>
                             Practice and discussion oriented group that helps people calm and soothe their nervous systems, looking at different aspects of mindfulness and sharing thoughts about the practice.
